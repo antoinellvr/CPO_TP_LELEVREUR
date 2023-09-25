7a46d1a3f836a98dab2de51cb91371f69752dc59
Antoine Le Levreur <antoine.lelevreur@epfedu.fr>
2023-09-25 11:13:23 +0200

    ajoute exo1 exo2 (exo2 fin qui beug) et calculator

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *calculator
+ * @author alele
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("please enter the operator:\n 1) add\n 2) substract\n 3) multiply \n 4) divide \n 5) modulo");
+    }
+    
+}
---
+++ EXO2/src/exo2/EXO2.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo2;
+
+import java.util.Scanner;
+
+/**
+ *exo 2 decouverte java
+ * @author alele
+*/
+public class EXO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // TODO code application logic here
+//Declaration des variables
+        // nombre dentiers a additionner
+        // nombre dentiers a additionner
+int result; // resultat
+int ind; //indice
+int nb;
+//nb=5;
+Scanner sc = new Scanner(System.in);
+System.out.println("\n Entrer le nombre :");
+nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+result=0;
+// Addition des nb premiers entiers
+ind=1;
+while (ind <= nb) {
+result=result+ind;
+}
+// Affichage du resultat
+System.out.println();
+System.out.println("La somme des "+ nb + "entiers est: "+result);
+}
+    }
+    
