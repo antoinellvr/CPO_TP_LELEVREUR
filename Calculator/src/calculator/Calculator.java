ea52a3534df687f6439d2af673d1093fd755d4c4
Antoine Le Levreur <antoine.lelevreur@epfedu.fr>
2023-09-26 16:54:23 +0200

    ajoute exo1 exo2 (exo2 fin qui beug) et calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -4,8 +4,11 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
- *calculator
+ * calculator
+ *
  * @author alele
  */
 public class Calculator {
@@ -15,7 +18,51 @@ public class Calculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("please enter the operator:\n 1) add\n 2) substract\n 3) multiply \n 4) divide \n 5) modulo");
+        System.out.println("Rentre l'operateur:\n 1) add\n 2) substract\n 3) multiply \n 4) divide \n 5) modulo ");
+
+        try ( Scanner scanner = new Scanner(System.in)) {
+            int operator = scanner.nextInt();
+
+            System.out.println("Rentre le premier nombre:");
+            double num1 = scanner.nextDouble();
+
+            System.out.println("Rentre le deuxieme nombre:");
+            double num2 = scanner.nextDouble();
+
+            double result = 0.0;
+
+            switch (operator) {
+                case 1 ->
+                    result = num1 + num2;
+                case 2 ->
+                    result = num1 - num2;
+                case 3 ->
+                    result = num1 * num2;
+                case 4 -> {
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Error: Division par zero.");
+                        System.exit(1);
+                    }
+                }
+                case 5 -> {
+                    if (num2 != 0) {
+                        result = num1 % num2;
+                    } else {
+                        System.out.println("Error: Modulo par zero.");
+                        System.exit(1);
+                    }
+                }
+                default -> {
+                    System.out.println("Invalid operation.");
+                    System.exit(1);
+                }
+            }
+
+            System.out.println("The result is: " + result);
+        }
     }
-    
+}
+
 }
